159f5d329b73f12596c01e1249b48d1d55225c5c
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-08-19 22:48:50 +0800

    先删除PostUp Down再添加

--- wgcf_postup_postdown.sh
+++ wgcf_postup_postdown.sh
@@ -1,4 +1,6 @@
 wg-quick down wgcf
+sed -i '/PostUp/d' /etc/wireguard/wgcf.conf
+sed -i '/PostDown/d' /etc/wireguard/wgcf.conf
 sed -i "7 s/^/PostUp = ip -4 rule add from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
 sed -i "8 s/^/PostDown = ip -4 rule delete from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
 wg-quick up wgcf
