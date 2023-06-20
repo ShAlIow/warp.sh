8348d52e3b2e8308ca4196a15d259e76f30b0a72
crazypeace <txican@gmail.com>
2023-06-21 02:56:08 +0800

    同时支持ipv4 ipv6

--- wgcf_postup_postdown.sh
+++ wgcf_postup_postdown.sh
@@ -1,6 +1,22 @@
 wg-quick down wgcf
+
 sed -i '/PostUp/d' /etc/wireguard/wgcf.conf
 sed -i '/PostDown/d' /etc/wireguard/wgcf.conf
-sed -i "7 s/^/PostUp = ip -4 rule add from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
-sed -i "8 s/^/PostDown = ip -4 rule delete from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
+#sed -i "7 s/^/PostUp = ip -4 rule add from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
+#sed -i "8 s/^/PostDown = ip -4 rule delete from $(ip route get 1.1.1.1 | grep -oP 'src \K\S+') lookup main\n/" /etc/wireguard/wgcf.conf
+
+# IPv4
+ipv4if=$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')
+if [[ -n $ipv4if ]]; then 
+  sed -i "/\[Interface\]/ aPostDown = ip -4 rule delete from ${ipv4if} lookup main" /etc/wireguard/wgcf.conf
+  sed -i "/\[Interface\]/ aPostUp = ip -4 rule add from ${ipv4if} lookup main" /etc/wireguard/wgcf.conf
+fi
+
+# IPv6
+ipv6if=$(ip route get 2606:4700:4700::1111 | grep -oP 'src \K\S+')
+if [[ -n $ipv6if ]]; then
+  sed -i "/\[Interface\]/ aPostDown = ip -4 rule delete from ${ipv6if} lookup main" /etc/wireguard/wgcf.conf
+  sed -i "/\[Interface\]/ aPostUp = ip -4 rule add from ${ipv6if} lookup main" /etc/wireguard/wgcf.conf
+fi
+
 wg-quick up wgcf
